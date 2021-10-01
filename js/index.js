865f66994e8b044390785bc4711943e91d318654
lovergard76 <83507205+lovergard76@users.noreply.github.com>
2021-10-01 12:17:38 -0400

    jQuery HW19

--- js/index.js
+++ js/index.js
@@ -9,4 +9,28 @@ setTimeout(fade, 3000);
 $("#toTop").click(function(){
     $("html, body").animate({ scrollTop: 0}, "slow");
     return false;
-})
+});
+
+$(".workGTG").mouseover(function(){
+    $(".activeGTG").show("slow");
+});
+$(".workGTG").mouseout(function(){
+    $(".activeGTG").hide("slow");
+});
+
+
+
+
+$(".workE").mouseover(function(){
+    $(".activeEnergy").show("slow");
+});
+$(".workE").mouseout(function(){
+    $(".activeEnergy").hide("slow");
+});
+
+$(".workHH").mouseover(function(){
+    $(".activeHH").show("slow");
+});
+$(".workHH").mouseout(function(){
+    $(".activeHH").hide("slow");
+});
