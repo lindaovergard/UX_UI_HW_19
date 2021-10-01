e78fd73124ab787fb93f8c7856c59b6833b37b6d
lovergard76 <83507205+lovergard76@users.noreply.github.com>
2021-09-30 23:26:02 -0400

    jquery and navbar

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 console.log("Your index.js file is loaded correctly!");
 
-
 function fade(){
     $(".preloader").fadeOut("slow");
 }
-setTimeout (fade, 3000);
+
+setTimeout(fade, 3000);
+
+$("#toTop").click(function(){
+    $("html, body").animate({ scrollTop: 0}, "slow");
+    return false;
+})
